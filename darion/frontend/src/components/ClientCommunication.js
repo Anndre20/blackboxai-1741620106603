42711e16e3a60c8e3e6cd6172d82ff846161a770
Anndre20 <113474575+Anndre20@users.noreply.github.com>
2025-03-10 11:22:00 -0400

    Add darion/frontend/src/components/ClientCommunication.js 1741620119623

---
+++ darion/frontend/src/components/ClientCommunication.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ClientCommunication() {
+    return (
+        <div>
+            <h2>Client Communication</h2>
+            <p>Customize client update reports and manage communications.</p>
+        </div>
+    );
+}
+
+export default ClientCommunication;
