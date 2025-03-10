e9a31e77143fdd533ba2f70f7ad3dd5719e8d215
Anndre20 <113474575+Anndre20@users.noreply.github.com>
2025-03-10 11:22:13 -0400

    Add darion/backend/modules/email_handler.py 1741620133258

---
+++ darion/backend/modules/email_handler.py
@@ -0,0 +1,9 @@
+def send_email_via_gmail():
+    """Send email using Gmail API."""
+    # Implementation will go here
+    pass
+
+def send_email_via_outlook():
+    """Send email using Microsoft Graph API."""
+    # Implementation will go here
+    pass
